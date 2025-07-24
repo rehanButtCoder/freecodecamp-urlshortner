2e622b261d148a4bb6161275e1d1a36ff750ecf3
Rehan Butt <rehan.butt15555@gmail.com>
2025-07-24 16:53:48 +0500

    a little fix for shortner

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dns = require('dns');
+const url = require('url');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -14,15 +15,15 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', function (req, res) {
+app.post('/api/shorturl', function(req, res) {
   const originalUrl = req.body.url;
 
   const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
@@ -52,17 +53,17 @@ app.post('/api/shorturl', function (req, res) {
   });
 });
 
-app.get('/api/shorturl/:short_url', function (req, res) {
-  const shortUrl = req.params.short_url;
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = parseInt(req.params.short_url);
   const originalUrl = urlDatabase[shortUrl];
 
   if (!originalUrl) {
-    return res.json({ error: 'No short URL found' });
+    return res.json({ error: 'No short URL found for the provided input' });
   }
 
   res.redirect(originalUrl);
 });
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
